1c1e7c03d6666eafba09b9b5f0caf571c7976f11
wbamberg <will@bootbonnet.ca>
2023-05-30 10:51:35 -0700

    Prettier

--- installprompt/app.js
+++ installprompt/app.js
@@ -4,26 +4,26 @@ let installPrompt = null;
 const installButton = document.querySelector("#install");
 
 window.addEventListener("beforeinstallprompt", (event) => {
-	console.log("beforeinstallprompt fired");
-	event.preventDefault();
-	installPrompt = event;
-	installButton.removeAttribute("hidden");
+  console.log("beforeinstallprompt fired");
+  event.preventDefault();
+  installPrompt = event;
+  installButton.removeAttribute("hidden");
 });
 
 installButton.addEventListener("click", async () => {
-	if (!installPrompt) {
-		return;
-	}
-	const result = await installPrompt.prompt();
-	console.log(`Install prompt was: ${result.outcome}`);
-	disableInAppInstallPrompt();
+  if (!installPrompt) {
+    return;
+  }
+  const result = await installPrompt.prompt();
+  console.log(`Install prompt was: ${result.outcome}`);
+  disableInAppInstallPrompt();
 });
 
 window.addEventListener("appinstalled", () => {
-	disableInAppInstallPrompt();
+  disableInAppInstallPrompt();
 });
 
 function disableInAppInstallPrompt() {
-	installPrompt = null;
-	installButton.setAttribute("hidden", "");
+  installPrompt = null;
+  installButton.setAttribute("hidden", "");
 }
